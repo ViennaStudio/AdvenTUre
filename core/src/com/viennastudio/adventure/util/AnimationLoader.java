59af140c92eabf2d18086c82ab35cb74e4fe2a1e
Julian Leucker <leuckerj@gmail.com>
2022-08-18 19:42:17 +0200

    :sparkles: Create utility classes for animation handling

---
+++ core/src/com/viennastudio/adventure/util/AnimationLoader.java
@@ -0,0 +1,29 @@
+package com.viennastudio.adventure.util;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.viennastudio.adventure.Constants;
+
+public class AnimationLoader {
+    public static Animation<TextureRegion> load(String fileName) {
+        return load(new Texture(Gdx.files.internal(fileName)));
+    }
+
+    public static Animation<TextureRegion> load(Texture texture) {
+        TextureRegion[][] tmp = TextureRegion.split(
+                texture,
+                texture.getWidth() / Constants.ANIMATION_COLS,
+                Constants.ANIMATION_SIZE
+        );
+        TextureRegion[] frames = new TextureRegion[Constants.ANIMATION_COLS * texture.getHeight() / Constants.ANIMATION_SIZE];
+        int index = 0;
+        for (int i = 0; i < texture.getHeight() / Constants.ANIMATION_SIZE; i++) {
+            for (int j = 0; j < Constants.ANIMATION_COLS; j++) {
+                frames[index++] = tmp[i][j];
+            }
+        }
+        return new Animation<>(Constants.ANIMATION_INTERVAL, frames);
+    }
+}
