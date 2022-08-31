6416613028b41d63e34be3119718a926e60cd861
Julian Leucker <leuckerj@gmail.com>
2022-08-31 21:53:04 +0200

    :sparkles: Use two stages in Game class

--- core/src/com/viennastudio/adventure/AdvenTUreGame.java
+++ core/src/com/viennastudio/adventure/AdvenTUreGame.java
@@ -2,11 +2,15 @@ package com.viennastudio.adventure;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.viennastudio.adventure.entities.AnimationMap;
 import com.viennastudio.adventure.entities.Player;
 import com.viennastudio.adventure.screens.MainMenuScreen;
@@ -15,56 +19,72 @@ import com.viennastudio.adventure.util.AnimationLoader;
 
 public class AdvenTUreGame extends Game {
 
-	public SpriteBatch batch;
-	public BitmapFont font;
-	public Player player;
+    public SpriteBatch batch;
+    public SpriteBatch staticBatch;
+    public Stage gameStage;
+    public Stage UIStage;
+    public Viewport gameViewport;
+    public Viewport UIViewport;
 
-	private Texture playerStillTexture;
-	private Texture playerUpTexture;
-	private Texture playerDownTexture;
-	private Texture playerLeftTexture;
-	private Texture playerRightTexture;
+    public BitmapFont font;
+    public Player player;
 
-	public void create() {
-		batch = new SpriteBatch();
-		font = new BitmapFont(Gdx.files.internal("fonts/Silkscreen.fnt")); // use Silkscreen.fnt for the font
+    private Texture playerStillTexture;
+    private Texture playerUpTexture;
+    private Texture playerDownTexture;
+    private Texture playerLeftTexture;
+    private Texture playerRightTexture;
 
-		playerStillTexture = new Texture("player/PlayerStill.png");
-		playerDownTexture = new Texture("player/PlayerDown.png");
-		playerUpTexture = new Texture("player/PlayerUp.png");
-		playerLeftTexture = new Texture("player/PlayerLeft.png");
-		playerRightTexture = new Texture("player/PlayerRight.png");
+    public void create() {
+        batch = new SpriteBatch();
+        staticBatch = new SpriteBatch();
+        gameStage = new Stage();
+        UIStage = new Stage();
 
-		Animation<TextureRegion> playerStillAnimation = AnimationLoader.load(playerStillTexture);
-		Animation<TextureRegion> playerUpAnimation = AnimationLoader.load(playerUpTexture);
-		Animation<TextureRegion> playerLeftAnimation = AnimationLoader.load(playerLeftTexture);
-		Animation<TextureRegion> playerDownAnimation = AnimationLoader.load(playerDownTexture);
-		Animation<TextureRegion> playerRightAnimation = AnimationLoader.load(playerRightTexture);
+        InputMultiplexer multiplexer = new InputMultiplexer();
+        multiplexer.addProcessor(gameStage);
+        multiplexer.addProcessor(UIStage);
+        Gdx.input.setInputProcessor(multiplexer);
 
-		AnimationMap animationMap = new AnimationMap(
-				playerStillAnimation,
-				playerLeftAnimation,
-				playerRightAnimation,
-				playerUpAnimation,
-				playerDownAnimation
-		);
+        font = new BitmapFont(Gdx.files.internal("fonts/Silkscreen.fnt")); // use Silkscreen.fnt for the font
 
-		player = new Player(animationMap);
+        playerStillTexture = new Texture("player/PlayerStill.png");
+        playerDownTexture = new Texture("player/PlayerDown.png");
+        playerUpTexture = new Texture("player/PlayerUp.png");
+        playerLeftTexture = new Texture("player/PlayerLeft.png");
+        playerRightTexture = new Texture("player/PlayerRight.png");
 
-		setScreen(new MainMenuScreen(this));
-	}
+        Animation<TextureRegion> playerStillAnimation = AnimationLoader.load(playerStillTexture);
+        Animation<TextureRegion> playerUpAnimation = AnimationLoader.load(playerUpTexture);
+        Animation<TextureRegion> playerLeftAnimation = AnimationLoader.load(playerLeftTexture);
+        Animation<TextureRegion> playerDownAnimation = AnimationLoader.load(playerDownTexture);
+        Animation<TextureRegion> playerRightAnimation = AnimationLoader.load(playerRightTexture);
 
-	public void render() {
-		super.render(); // important!
-	}
+        AnimationMap animationMap = new AnimationMap(
+                playerStillAnimation,
+                playerLeftAnimation,
+                playerRightAnimation,
+                playerUpAnimation,
+                playerDownAnimation
+        );
 
-	public void dispose() {
-		playerStillTexture.dispose();
-		playerLeftTexture.dispose();
-		playerRightTexture.dispose();
-		playerUpTexture.dispose();
-		playerDownTexture.dispose();
-		batch.dispose();
-		font.dispose();
-	}
+        player = new Player(animationMap);
+
+        setScreen(new MainMenuScreen(this));
+    }
+
+    public void render() {
+        super.render(); // important!
+    }
+
+    public void dispose() {
+        playerStillTexture.dispose();
+        playerLeftTexture.dispose();
+        playerRightTexture.dispose();
+        playerUpTexture.dispose();
+        playerDownTexture.dispose();
+        if (batch != null) batch.dispose();
+        if (staticBatch != null) staticBatch.dispose();
+        font.dispose();
+    }
 }
