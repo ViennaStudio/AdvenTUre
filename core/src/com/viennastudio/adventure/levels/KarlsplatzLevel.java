d72e0db120b4fbc91a9c2d36d75399bbd71790e5
Julian Leucker <leuckerj@gmail.com>
2022-09-02 16:32:49 +0200

    :bug: Correctly resize chatbox

--- core/src/com/viennastudio/adventure/levels/KarlsplatzLevel.java
+++ core/src/com/viennastudio/adventure/levels/KarlsplatzLevel.java
@@ -1,12 +1,14 @@
 package com.viennastudio.adventure.levels;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Align;
 import com.viennastudio.adventure.AdvenTUreGame;
 import com.viennastudio.adventure.hud.ChatBox;
 import com.viennastudio.adventure.level.Level;
 import com.viennastudio.adventure.level.LevelConfig;
 
 public class KarlsplatzLevel extends Level {
+    ChatBox chatBox;
 
     public KarlsplatzLevel(AdvenTUreGame game) {
         super(game, new LevelConfig(
@@ -22,7 +24,13 @@ public class KarlsplatzLevel extends Level {
     protected void afterShow() {
         //ChatBox
         String text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
-        ChatBox chatBox = new ChatBox(game.skin, "default","Julian", text);
+        chatBox = new ChatBox(game.skin, "default","Julian", text);
         game.UIStage.addActor(chatBox);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        chatBox.setPosition(game.UIStage.getWidth() / 2, 0, Align.bottom | Align.center);
     }
 }
